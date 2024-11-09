19faaeffe0ea6f73171a38b32af865931e4aff23
Lorena Medeiros <lomedeiros@proton.me>
2024-11-09 17:35:53 -0300

    feat: add Product service and repository

---
+++ src/main/java/io/github/lsmcodes/inventorymanager/service/product/impl/ProductServiceImpl.java
@@ -0,0 +1,100 @@
+package io.github.lsmcodes.inventorymanager.service.product.impl;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import io.github.lsmcodes.inventorymanager.dto.product.ProductRequestDTO;
+import io.github.lsmcodes.inventorymanager.dto.product.ProductResponseDTO;
+import io.github.lsmcodes.inventorymanager.model.product.Product;
+import io.github.lsmcodes.inventorymanager.repository.ProductRepository;
+import io.github.lsmcodes.inventorymanager.service.product.ProductService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public ProductResponseDTO createProduct(ProductRequestDTO dto) {
+        verifyProductDoesNotExist(dto.getCode());
+        Product newProduct = dto.toEntity();
+        return productRepository.save(newProduct).toResponseDTO();
+    }
+
+    @Override
+    public ProductResponseDTO findProductById(UUID id) {
+        Product foundProduct = findProductOrThrow(id);
+        return foundProduct.toResponseDTO();
+    }
+
+    @Override
+    public ProductResponseDTO findProductByCode(String code) {
+        Product foundProduct = productRepository.findByCode(code)
+                .orElseThrow(() -> new EntityNotFoundException());
+        return foundProduct.toResponseDTO();
+    }
+
+    @Override
+    public Page<ProductResponseDTO> findProductsByName(String name, Pageable pageable) {
+        Page<Product> foundProducts = productRepository.findByNameContainingIgnoreCase(name, pageable);
+        return foundProducts.map(product -> product.toResponseDTO());
+    }
+
+    @Override
+    public Page<ProductResponseDTO> findAllProducts(Pageable pageable) {
+        Page<Product> foundProducts = productRepository.findAll(pageable);
+        return foundProducts.map(product -> product.toResponseDTO());
+    }
+
+    @Override
+    public ProductResponseDTO updateProduct(UUID id, ProductRequestDTO dto) {
+        verifyProductExists(id);
+        verifyProductDoesNotExist(dto.getCode());
+        Product updatedProduct = dto.toEntity();
+        updatedProduct.setId(id);
+        return productRepository.save(updatedProduct).toResponseDTO();
+    }
+
+    @Override
+    public void deleteProductById(UUID id) {
+        verifyProductExists(id);
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public ProductResponseDTO increaseProductQuantity(UUID id, int quantity) {
+        Product product = findProductOrThrow(id);
+        product.setQuantity(product.getQuantity() + quantity);
+        return productRepository.save(product).toResponseDTO();
+    }
+
+    @Override
+    public ProductResponseDTO decreaseProductQuantity(UUID id, int quantity) {
+        Product product = findProductOrThrow(id);
+        product.setQuantity(product.getQuantity() - quantity);
+        return productRepository.save(product).toResponseDTO();
+    }
+
+    private void verifyProductDoesNotExist(String code) {
+        if (productRepository.existsByCode(code)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void verifyProductExists(UUID id) {
+        if (!productRepository.existsById(id)) {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    private Product findProductOrThrow(UUID id) {
+        return productRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    }
+
+}
---
+++ src/test/java/io/github/lsmcodes/inventorymanager/service/product/impl/ProductServiceImplTest.java
@@ -0,0 +1,163 @@
+package io.github.lsmcodes.inventorymanager.service.product.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+
+import io.github.lsmcodes.inventorymanager.dto.product.ProductRequestDTO;
+import io.github.lsmcodes.inventorymanager.dto.product.ProductResponseDTO;
+import io.github.lsmcodes.inventorymanager.model.product.Product;
+import io.github.lsmcodes.inventorymanager.repository.ProductRepository;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceImplTest {
+
+    private final UUID id = UUID.randomUUID();
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productServiceImpl;
+
+    @Test
+    @DisplayName("Should call the repository save method when creating a product")
+    public void createProductTest() {
+        ProductRequestDTO requestDTO = getValidRequestDTO();
+        when(productRepository.save(any(Product.class))).thenReturn(requestDTO.toEntity());
+
+        ProductResponseDTO responseDTO = productServiceImpl.createProduct(requestDTO);
+
+        assertThat(responseDTO).usingRecursiveComparison()
+                .ignoringFields("id").isEqualTo(requestDTO);
+    }
+
+    @Test
+    @DisplayName("Should call the repository findById method when searching a product by id")
+    public void findProductByIdTest() {
+        ProductRequestDTO requestDTO = getValidRequestDTO();
+        when(productRepository.findById(id)).thenReturn(Optional.of(requestDTO.toEntity()));
+
+        ProductResponseDTO responseDTO = productServiceImpl.findProductById(id);
+
+        assertThat(responseDTO).usingRecursiveComparison()
+                .ignoringFields("id").isEqualTo(requestDTO);
+    }
+
+    @Test
+    @DisplayName("Should call the repository findByCode method when searching a product by code")
+    public void findProductByCodeTest() {
+        ProductRequestDTO requestDTO = getValidRequestDTO();
+        String code = requestDTO.getCode();
+        when(productRepository.findByCode(code)).thenReturn(Optional.of(requestDTO.toEntity()));
+
+        ProductResponseDTO responseDTO = productServiceImpl.findProductByCode(code);
+
+        assertThat(responseDTO).usingRecursiveComparison()
+                .ignoringFields("id").isEqualTo(requestDTO);
+    }
+
+    @Test
+    @DisplayName("Should call the repository findByNameContainingIgnoreCase method when searching products by name")
+    public void findProductsByNameTest() {
+        String name = "notebook";
+        Pageable pageable = PageRequest.of(0, 10);
+        Product product = getValidRequestDTO().toEntity();
+        when(productRepository.findByNameContainingIgnoreCase(name, pageable))
+                .thenReturn(new PageImpl<>(List.of(product)));
+
+        Page<ProductResponseDTO> foundPage = productServiceImpl.findProductsByName(name, pageable);
+
+        assertThat(product).usingRecursiveComparison().isEqualTo(foundPage.getContent().getFirst());
+    }
+
+    @Test
+    @DisplayName("Should call the repository findAll method when searching all products")
+    public void findAllProductsTest() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Product product = getValidRequestDTO().toEntity();
+        when(productRepository.findAll(pageable))
+                .thenReturn(new PageImpl<>(List.of(product)));
+
+        Page<ProductResponseDTO> foundPage = productServiceImpl.findAllProducts(pageable);
+
+        assertThat(product).usingRecursiveComparison().isEqualTo(foundPage.getContent().getFirst());
+    }
+
+    @Test
+    @DisplayName("Should call the repository save method when updating a product")
+    public void updateProductTest() {
+        ProductRequestDTO requestDTO = getValidRequestDTO();
+        when(productRepository.existsById(id)).thenReturn(true);
+        when(productRepository.save(any(Product.class))).thenReturn(requestDTO.toEntity());
+
+        ProductResponseDTO responseDTO = productServiceImpl.updateProduct(id, requestDTO);
+
+        assertThat(responseDTO).usingRecursiveComparison()
+                .ignoringFields("id").isEqualTo(requestDTO);
+    }
+
+    @Test
+    @DisplayName("Should call the repository deleteById method when deleting a product by id")
+    public void deleteProductByIdTest() {
+        when(productRepository.existsById(id)).thenReturn(true);
+
+        productServiceImpl.deleteProductById(id);
+
+        verify(productRepository).deleteById(id);
+    }
+
+    @Test
+    @DisplayName("Should call the repository findById and save methods when increasing a product quantity")
+    public void increaseProductQuantity() {
+        ProductRequestDTO requestDTO = getValidRequestDTO();
+        when(productRepository.existsById(id)).thenReturn(true);
+        when(productRepository.findById(id)).thenReturn(Optional.of(requestDTO.toEntity()));
+
+        requestDTO.setQuantity(14);
+        when(productRepository.save(any(Product.class))).thenReturn(requestDTO.toEntity());
+
+        ProductResponseDTO productResponseDTO = productServiceImpl.decreaseProductQuantity(id, 5);
+
+        assertThat(productResponseDTO.getQuantity()).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("Should call the repository FindById and save methods when decreasing a product quantity")
+    public void decreaseProductQuantity() {
+        ProductRequestDTO requestDTO = getValidRequestDTO();
+        when(productRepository.existsById(id)).thenReturn(true);
+        when(productRepository.findById(id)).thenReturn(Optional.of(requestDTO.toEntity()));
+
+        requestDTO.setQuantity(6);
+        when(productRepository.save(any(Product.class))).thenReturn(requestDTO.toEntity());
+
+        ProductResponseDTO productResponseDTO = productServiceImpl.decreaseProductQuantity(id, 3);
+
+        assertThat(productResponseDTO.getQuantity()).isEqualTo(6);
+    }
+
+    private ProductRequestDTO getValidRequestDTO() {
+        return new ProductRequestDTO("00000000", "Spiral Notebook", new BigDecimal(14.99), 9);
+    }
+
+}
