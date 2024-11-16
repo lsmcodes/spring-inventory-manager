082a0f881311658392e9864be2bdeb7adbf04690
Lorena Medeiros <lomedeiros@proton.me>
2024-11-16 14:36:58 -0300

    refactor: simplify tests by replacing redundant method calls

--- src/test/java/io/github/lsmcodes/inventorymanager/controller/inventoryevent/InventoryEventControllerTest.java
+++ src/test/java/io/github/lsmcodes/inventorymanager/controller/inventoryevent/InventoryEventControllerTest.java
@@ -1,12 +1,18 @@
 package io.github.lsmcodes.inventorymanager.controller.inventoryevent;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,8 +25,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,66 +37,75 @@ import io.github.lsmcodes.inventorymanager.service.inventoryevent.InventoryEvent
 @WebMvcTest(InventoryEventController.class)
 public class InventoryEventControllerTest {
 
-    @Autowired
-    private ObjectMapper objectMapper;
+        @Autowired
+        private ObjectMapper objectMapper;
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @MockBean
-    private InventoryEventService inventoryEventService;
+        @MockBean
+        private InventoryEventService inventoryEventService;
 
-    @Test
-    @DisplayName("Should return created status and inventory event")
-    public void createInventoryEventTest() throws JsonProcessingException, Exception {
-        UUID productId = UUID.randomUUID();
-        InventoryEventRequestDTO requestDTO = getValidRequestDTO();
+        @Test
+        @DisplayName("Should return created status and inventory event")
+        public void createInventoryEventTest() throws JsonProcessingException, Exception {
+                UUID productId = UUID.randomUUID();
+                InventoryEventRequestDTO requestDTO = getValidRequestDTO();
 
-        Mockito.when(inventoryEventService.createInventoryEvent(Mockito.any(UUID.class), Mockito.any(InventoryEventRequestDTO.class))).thenReturn(requestDTO.toEntity().toResponseDTO());
+                when(inventoryEventService.createInventoryEvent(any(UUID.class), any(InventoryEventRequestDTO.class)))
+                                .thenReturn(requestDTO.toEntity().toResponseDTO());
 
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/api/inventory-events/{product-id}", productId)
-                        .content(objectMapper.writeValueAsString(requestDTO))
-                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.eventType").value(requestDTO.getEventType()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.quantity").value(requestDTO.getQuantity()));
-    }
+                mockMvc.perform(
+                                post("/api/inventory-events/{product-id}", productId)
+                                                .content(objectMapper.writeValueAsString(requestDTO))
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isCreated())
+                                .andExpect(jsonPath("$.eventType")
+                                                .value(requestDTO.getEventType()))
+                                .andExpect(jsonPath("$.quantity")
+                                                .value(requestDTO.getQuantity()));
+        }
 
-    @Test
-    @DisplayName("Should return ok status and inventory event found by id")
-    public void findInventoryEventByIdTest() throws JsonProcessingException, Exception {
-        UUID id = UUID.randomUUID();
-        InventoryEventRequestDTO requestDTO = getValidRequestDTO();
+        @Test
+        @DisplayName("Should return ok status and inventory event found by id")
+        public void findInventoryEventByIdTest() throws JsonProcessingException, Exception {
+                UUID id = UUID.randomUUID();
+                InventoryEventRequestDTO requestDTO = getValidRequestDTO();
 
-        Mockito.when(inventoryEventService.findInventoryEventById(id))
-                .thenReturn(requestDTO.toEntity().toResponseDTO());
+                when(inventoryEventService.findInventoryEventById(id))
+                                .thenReturn(requestDTO.toEntity().toResponseDTO());
 
-        mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/inventory-events/{id}", id).accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.eventType").value(requestDTO.getEventType()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.quantity").value(requestDTO.getQuantity()));
-    }
+                mockMvc.perform(
+                                get("/api/inventory-events/{id}", id)
+                                                .accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.eventType")
+                                                .value(requestDTO.getEventType()))
+                                .andExpect(jsonPath("$.quantity")
+                                                .value(requestDTO.getQuantity()));
+        }
 
-    @Test
-    @DisplayName("Should return ok status and all inventory events")
-    public void findInventoryEventsTest() throws Exception {
-        InventoryEventRequestDTO requestDTO = getValidRequestDTO();
-        Pageable pageable = PageRequest.of(0, 20, Sort.by(Direction.ASC, "createdAt"));
+        @Test
+        @DisplayName("Should return ok status and all inventory events")
+        public void findInventoryEventsTest() throws Exception {
+                InventoryEventRequestDTO requestDTO = getValidRequestDTO();
+                Pageable pageable = PageRequest.of(0, 20, Sort.by(Direction.ASC, "createdAt"));
 
-        Mockito.when(inventoryEventService.findAllInventoryEvents(pageable))
-                .thenReturn(new PageImpl<>(List.of(requestDTO.toEntity().toResponseDTO())));
+                when(inventoryEventService.findAllInventoryEvents(pageable))
+                                .thenReturn(new PageImpl<>(List.of(requestDTO.toEntity().toResponseDTO())));
 
-        mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/inventory-events").accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].eventType").value(requestDTO.getEventType()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].quantity").value(requestDTO.getQuantity()));
-    }
+                mockMvc.perform(
+                                get("/api/inventory-events").accept(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.content[0].eventType")
+                                                .value(requestDTO.getEventType()))
+                                .andExpect(jsonPath("$.content[0].quantity")
+                                                .value(requestDTO.getQuantity()));
+        }
 
-    private InventoryEventRequestDTO getValidRequestDTO() {
-        return new InventoryEventRequestDTO("STOCK_IN", 4);
-    }
+        private InventoryEventRequestDTO getValidRequestDTO() {
+                return new InventoryEventRequestDTO("STOCK_IN", 4);
+        }
 
 }
